003243a1bd64ec6b44fa389ea3cb25379ffed034
brindha1991 <109019333+brindha1991@users.noreply.github.com>
2022-07-10 09:39:03 +0100

    Add files via upload

---
+++ Browser.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Browser {
+	
+	public static void main(String[] args) {
+		float version = 100.2f;
+		String name = "firefox";
+		
+		System.out.println("This is my first script");
+	}
+}
+	
+
+
---
+++ Car.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'm';
+		
+		//print
+		System.out.println(version);
+		System.out.println(name);
+		System.out.println("This is my first script");
+	}
+}
+
+
+	
+		
+	
+	
+	
+	
+	
+	
+
+
